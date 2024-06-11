a3af1f5437521756cd697055eebd915bfdbf8e7e
ericapobre <107044913+ericapobre@users.noreply.github.com>
2024-06-11 11:54:12 -0300

    Primeira aula

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.querySelector("caixa-principal");
+const caixaPerguntas = document.querySelector("caixa-perguntas");
+const caixaAlternativas = document.querySelector("caixa-alternativa");
+const caixaResultados = document.querySelector("caixa-resultado");
+const textoResultado = document.querySelector("caixa-resultado");
+
+const perguntas = [
+
+        enunciado: "Assim que saiu ",
+        alternativa: [
+            "Isso é assustador!",
+            "Isso é maravilhoso!"
+    ]
+    ,
+    enunciado: "com descoberta desta tecnologia, chamada inteligencia Artificial (IA),",
+    alternativas:
